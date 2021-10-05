6d19a05368aba03aedaab43b7735e392d772cb05
Emmanuel Izilov <em5750@gmail.com>
2021-10-05 18:44:30 -0400

    added confirmation for UPDATE problem

--- invoice.sql
+++ invoice.sql
@@ -33,8 +33,11 @@ SELECT AVG(total) FROM invoice; -- 5.7063
 SELECT SUM(total) FROM invoice; -- 2351
 
 -- Update the invoice with an invoice_id of 5 to have a total order amount of 24.
+SELECT * FROM invoice WHERE invoice_id = 5; -- CHeck the value before updating
 
 UPDATE invoice SET total = 24 WHERE invoice_id = 5;
+
+SELECT * FROM invoice WHERE invoice_id = 5; -- Check the value after updating
 
 -- Delete the invoice with an invoice_id of 1.
 
